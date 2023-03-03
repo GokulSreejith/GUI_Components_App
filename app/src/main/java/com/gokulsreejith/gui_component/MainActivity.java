532d3f233d14577fc6fb5ffc5aacc366fd2a6d22
GokulSreejith <gokulsreejith22002@gmail.com>
2023-03-03 21:39:29 +0530

    First app developed

---
+++ app/src/main/java/com/gokulsreejith/gui_component/MainActivity.java
@@ -0,0 +1,56 @@
+package com.gokulsreejith.gui_component;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+    int i = 1;
+    float font = 24;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView t1 = (TextView) findViewById(R.id.textview1);
+        Button b1 = (Button) findViewById(R.id.button1);
+        b1.setOnClickListener(new View.OnClickListener(){
+            public  void onClick(View view) {
+                t1.setTextSize(font);
+                font = font + 4;
+                if(font == 40)
+                    font = 20;
+            }
+        });
+        Button b2 = (Button) findViewById(R.id.button2);
+        b2.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+
+                switch (i){
+                    case 1:
+                        t1.setTextColor(Color.parseColor("#0000FF"));
+                        break;
+                    case 2:
+                        t1.setTextColor(Color.parseColor("#00FF00"));
+                        break;
+                    case 3:
+                        t1.setTextColor(Color.parseColor("#FF0000"));
+                        break;
+                    case 4:
+                        t1.setTextColor(Color.parseColor("#800000"));
+                        break;
+                }
+                i++;
+                if(i == 5)
+                    i = 1;
+            }
+        });
+
+
+    }
+
+}
